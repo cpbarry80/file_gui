ad432ccbd3c29f52f591538b357d424539afc071
Biff Barry <cpbarry80@gmail.com>
2022-01-23 19:04:55 -0500

    setup py

--- missing_file_names.py
+++ missing_file_names.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter.messagebox import showinfo
 from tkinter import *
 
+# setup container window
 container = tk.Tk()
 container.geometry('600x400')
 container.title('missing files')
@@ -11,18 +12,18 @@ w = Label(container, text ='\n \n compare contents of old work and new work \n \
 w.pack()
 
 
-# populate listbox
+# define listbox and its contents
 folder = '/Users/biffbarry/Desktop/models'
 filelist = [fname for fname in os.listdir(folder)]
 list_items = tk.StringVar(value=filelist)
 listbox = tk.Listbox(listvariable=list_items, height=len(filelist), selectmode='browse')
 listbox.pack(fill='x')
 
-
+# define text box
 txt = Text(container, height = 200, width = 200)
 txt.pack()
 
-# pass selected listbox to message box so files are listed
+# get contents for text box
 def files_needed(event):
     folder_selected = listbox.get(listbox.curselection()[0])
     folder = f'/Users/biffbarry/Desktop/models/{folder_selected}'
@@ -31,8 +32,8 @@ def files_needed(event):
     for i in filelist:
         txt.insert('1.0', f'\t {i} \n')
 
-listbox.bind('<<ListboxSelect>>', files_needed)
 
+listbox.bind('<<ListboxSelect>>', files_needed)
 container.mainloop()
 
 
